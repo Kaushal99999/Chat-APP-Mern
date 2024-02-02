cfe77f3bc2e9dbc67d7f89a3136eb6633b83880d
Kaushal99999 <96714323+Kaushal99999@users.noreply.github.com>
2024-02-02 16:05:12 +0530

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,7 +4,7 @@ import UserContextProvider from './Context/ContextUser';
 import Routes from './Routes';
 
 function App() {
-  axios.defaults.baseURL='https://chat-app-mern-f58s.vercel.app';
+  axios.defaults.baseURL='https://chat-back-hzmz.onrender.com';
 axios.defaults.withCredentials=true; 
   return (
     <UserContextProvider>
